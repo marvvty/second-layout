bb6b06ae980dd53745bf6def70db4ed489221196
marvy <kopilec691@gmail.com>
2025-07-27 20:26:41 +0300

    new func

---
+++ js/main.js
@@ -0,0 +1,98 @@
+const btnScrollRight = document.getElementById("scroll-right");
+const btnScrollLeft = document.getElementById("scroll-left");
+
+const houseButton = document.getElementById("houseButton");
+const villaButton = document.getElementById("villaButton");
+const apartmentButton = document.getElementById("apartmentButton");
+
+const housesContainer = document.getElementById("houses");
+const villaContainer = document.getElementById("villa");
+const apartmentContainer = document.getElementById("apartment");
+
+btnScrollRight.addEventListener("click", buttonScrollRight);
+btnScrollLeft.addEventListener("click", buttonScrollLeft);
+
+houseButton.addEventListener("click", showHouses);
+villaButton.addEventListener("click", showVilla);
+apartmentButton.addEventListener("click", showApartment);
+
+function buttonScrollRight() {
+  const container = getVisibleCards();
+
+  container.scrollBy({
+    left: 340,
+    behavior: "smooth",
+  });
+}
+function buttonScrollLeft() {
+  const container = getVisibleCards();
+
+  container.scrollBy({
+    left: -340,
+    behavior: "smooth",
+  });
+}
+function checkScrollButtons() {
+  const container = getVisibleCards();
+  const scrollLeft = container.scrollLeft;
+  const scrollWidth = container.scrollWidth;
+  const clientWidth = container.clientWidth;
+  if (scrollLeft <= 0) {
+    btnScrollLeft.classList.remove("nav__controls-btn--active");
+    changeIcon("arrow-left", "asserts/images/icons/arrow-left.svg");
+  } else {
+    btnScrollLeft.classList.add("nav__controls-btn--active");
+    changeIcon("arrow-left", "asserts/images/icons/chevron-left.svg");
+  }
+  if (scrollLeft + clientWidth >= scrollWidth) {
+    btnScrollRight.classList.remove("nav__controls-btn--active");
+    changeIcon("arrow-right", "asserts/images/icons/chevron-right.svg");
+  } else {
+    btnScrollRight.classList.add("nav__controls-btn--active");
+    changeIcon("arrow-right", "asserts/images/icons/arrow-right.svg");
+  }
+}
+function changeIcon(iconId, iconPath) {
+  document.getElementById(iconId).src = iconPath;
+}
+function showHouses() {
+  housesContainer.classList.remove("property-card--hidden");
+  villaContainer.classList.add("property-card--hidden");
+  apartmentContainer.classList.add("property-card--hidden");
+  houseButton.classList.add("nav__btn--active");
+  villaButton.classList.remove("nav__btn--active");
+  apartmentButton.classList.remove("nav__btn--active");
+  checkScrollButtons();
+}
+function showVilla() {
+  housesContainer.classList.add("property-card--hidden");
+  villaContainer.classList.remove("property-card--hidden");
+  apartmentContainer.classList.add("property-card--hidden");
+  houseButton.classList.remove("nav__btn--active");
+  villaButton.classList.add("nav__btn--active");
+  apartmentButton.classList.remove("nav__btn--active");
+  checkScrollButtons();
+}
+function showApartment() {
+  housesContainer.classList.add("property-card--hidden");
+  villaContainer.classList.add("property-card--hidden");
+  apartmentContainer.classList.remove("property-card--hidden");
+  houseButton.classList.remove("nav__btn--active");
+  villaButton.classList.remove("nav__btn--active");
+  apartmentButton.classList.add("nav__btn--active");
+  checkScrollButtons();
+}
+function getVisibleCards() {
+  if (!housesContainer.classList.contains("property-card--hidden")) {
+    return housesContainer;
+  }
+  if (!villaContainer.classList.contains("property-card--hidden")) {
+    return villaContainer;
+  }
+  if (!apartmentContainer.classList.contains("property-card--hidden")) {
+    return apartmentContainer;
+  }
+}
+[housesContainer, villaContainer, apartmentContainer].forEach((container) => {
+  container.addEventListener("scroll", checkScrollButtons);
+});
